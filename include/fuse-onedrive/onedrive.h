2c5be4c441e5f1451f879b139b2cd64e7a202671
Rebecca Skinner <rebecca@rebeccaskinner.net>
2015-01-31 18:29:32 -0600

    first commit

---
+++ include/fuse-onedrive/onedrive.h
@@ -0,0 +1,8 @@
+#ifndef __LIB_ONEDRIVE_H__
+#define __LIB_ONEDRIVE_H__
+
+#ifndef __UNUSED (__attribute((UNUSED)))
+#define __UNUSED (__attribute((UNUSED)))
+#endif
+
+#endif
---
+++ lib/onedrive.c
@@ -0,0 +1,8 @@
+#include <fuse-onedrive/onedrive.h>
+
+int
+main(int argc, char** argv)
+{
+  printf("Hello, World!\n");
+  return 0;
+}
